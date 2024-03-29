4366c3e1093aca070f37c2f6764903a197ac71e2
Alexander Strand <alexanderstrand@Alexanders-Air.lan>
2024-03-29 17:44:57 +0100

    Bump

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} config */
+const config = {
+  content: ['./app/**/*.php', './resources/**/*.{php,vue,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'black': '#2A2B2A',
+        'yellow': '#FFBD52',
+        'white': '#F6EADF',
+        'red': '#D35845',
+        'green': '#154734',
+        'pink': '#EFBDBF'
+      }, // Extend Tailwind's default colors
+    },
+  },
+  plugins: [],
+};
+
+export default config;
