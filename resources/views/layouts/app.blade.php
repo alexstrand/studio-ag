04a1b58e5fe059c60ba0fc5dc05cce1620bb7501
Alex Strand <alexstrand@gmai.com>
2024-06-01 18:15:53 +0200

    GTM added.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html @php(language_attributes())>
   <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ERMHLT283W"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-ERMHLT283W');
+    </script>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @php(do_action('get_header'))
